69ce4e769219b3990802dae956557906dae43860
Shubham Bangar <shubhambangar2004@gmail.com>
2025-05-07 06:48:11 +0530

    Update hello.py

--- OneDrive/Desktop/ccpy/hello.py
+++ OneDrive/Desktop/ccpy/hello.py
@@ -1,2 +1,14 @@
-print("Shubham")
-print("hllo")
+# Greeting message
+print("Hello, welcome !")
+
+# Define variables
+name = "Rahul"
+num1 = 10
+num2 = 5
+
+# Display name
+print(f"Nice to meet you, {name}!")
+
+# Add two numbers
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}")
